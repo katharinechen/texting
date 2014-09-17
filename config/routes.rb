1246886558bf3e829811193cb11a185c8b270852
Katharine Chen & Diego Avalon <student@epicodus.com>
2014-09-17 13:39:27 -0700

    Add appropriate files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, path_name: {sign_in: "login", sign_out: "logout"}
 
+  root :to => "contact#index"
   resources :contacts do
     resources :messages
   end
+
 end
