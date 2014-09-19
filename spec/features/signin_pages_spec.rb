ff363a72e6f572f67da8a1a537fb9d3a7f65345d
Katharine Chen <katharinechen.ny@gmail.com>
2014-09-19 11:33:00 -0700

    Add Carybara testing for user signin process

--- spec/features/signin_pages_spec.rb
+++ spec/features/signin_pages_spec.rb
@@ -2,18 +2,22 @@ require 'rails_helper'
 
 describe "the signin process" do 
 
-  it "signs a user in who users the right password" do 
+   before :each do
     visit '/users/sign_in'
     user = create(:user)
+  end
+
+  it "signs a user in who users the right password" do 
     fill_in 'Email', :with => 'katharinechen.ny@gmail.com'
     fill_in 'Password', :with => '123456789'    
     click_button "Log in"
-    page.should have_content "Signed in successfully"
+    expect(page).to have_content "Signed in successfully"
   end 
 
-  # it "gives a user an error who uses the wrong password" do 
-
-  # end 
-
-
+  it "gives a user an error who uses the wrong password" do 
+    fill_in 'Email', :with => 'katharinechen.ny@gmail.com'
+    fill_in 'Password', :with => 'wrongpassword'    
+    click_button "Log in"
+    expect(page).to have_content "Invalid email or password"
+  end 
 end 
