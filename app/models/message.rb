89f7165be27cc8af51b9223a41f87e5595742b08
Katharine Chen <katharinechen.ny@gmail.com>
2014-09-19 11:36:43 -0700

    Fixed specs for Message object

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,10 +3,10 @@ class Message < ActiveRecord::Base
   belongs_to :user 
   belongs_to :contact 
 
-  # validates :contact_id, presence: true 
-  # validates :user_id, presence: true 
-  # validates :body, presence: true 
-  # validates :status, presence: true 
+  validates :contact_id, presence: true 
+  validates :user_id, presence: true 
+  validates :body, presence: true 
+  validates :status, presence: true 
 
   before_create :send_message
 
